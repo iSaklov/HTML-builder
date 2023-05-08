41f7b44b15ad9ce423e6dca61a82006085bce582
iSaklov <ivansaklov@gmail.com>
2023-05-08 22:22:18 +0200

    feat: add files-in-folder function

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs/promises')
+const path = require('path')
+
+const folderPath = path.join(__dirname, 'secret-folder')
+
+fs.readdir(folderPath, { withFileTypes: true })
+	.then((files) => {
+		for (const file of files) {
+			if (file.isFile()) {
+				const fileName = path.parse(file.name).name
+				const fileExtension = path.extname(file.name).slice(1)
+				fs.stat(path.join(folderPath, file.name))
+					.then((stats) => {
+						const fileSizeInBytes = stats.size
+						const fileSizeInKb = fileSizeInBytes / 1024
+						console.log(`${fileName}-${fileExtension}-${fileSizeInKb.toFixed(3)}kb`)
+					})
+					.catch((err) => {
+						console.error(`Не удалось получить информацию о файле: ${err}`)
+					})
+			}
+		}
+	})
+	.catch((err) => {
+		console.error(`Не удалось прочитать содержимое папки: ${err}`)
+	})
