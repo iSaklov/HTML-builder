a751efc73d0ec2efaf0140e9da126e18707e6c26
iSaklov <ivansaklov@gmail.com>
2023-05-08 23:47:00 +0200

    feat: add copy-directory function

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,15 +1,15 @@
-const fs = require('fs/promises')
+const { readdir, stat } = require('fs/promises')
 const path = require('path')
 
 const folderPath = path.join(__dirname, 'secret-folder')
 
-fs.readdir(folderPath, { withFileTypes: true })
+readdir(folderPath, { withFileTypes: true })
 	.then((files) => {
 		for (const file of files) {
 			if (file.isFile()) {
 				const fileName = path.parse(file.name).name
 				const fileExtension = path.extname(file.name).slice(1)
-				fs.stat(path.join(folderPath, file.name))
+				stat(path.join(folderPath, file.name))
 					.then((stats) => {
 						const fileSizeInBytes = stats.size
 						const fileSizeInKb = fileSizeInBytes / 1024
